46f6885c6d48b965cec41373a9a78be8ea57383c
Pieter Henning <pieterhenning.work@gmail.com>
2022-02-17 22:01:54 +0200

    edit readme and add html and js files

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Rock-Paper-Scissors
 The Odin Project Task to make rock paper scissors game that is run in the console against the computer
+
+the user is prompted to give a input of rock, paper or scissors, this input is checked to see if it is valid. The pogram also randoms a answer for the computer. the answers are compared and the winner gets a point and then after 5 rounds the player with the most points win.
+
+rock beats scissors
+scissors beats paper
+paper beats rock
+
+check valid entry
+    rock, paper or scissors
+    if the entry is not valid the palyer is propted again
+check if player has rock   
+    does coputer have scissor or paper 
+        if paper
+            computer wins
+        if scissor
+            player wins
+        return output           
+else does player have scissor           
+    does coputer have rock or paper 
+        if rock
+            computer wins
+        if paper
+            player wins
+        return output    
+else does player have paper           
+    does coputer have rock or scissor 
+        if paper
+            computer wins
+        if rock
+            player wins  
+        return output   
+  (each win gets that player a point)      
+Check who has the higher score and they win
---
+++ Script.js
@@ -0,0 +1,130 @@
+
+function computerPlay (){
+    let selector = 0;
+    let computerAnswer = 'rock';
+    
+    
+        selector = Math.floor(Math.random()*3);
+        
+    //console.log('selector: ' + selector);
+
+    if (selector  == 0){
+        computerAnswer = 'Rock';
+    } else if(selector == 1){
+        computerAnswer = 'Paper'
+    } else if(selector == 2){
+        computerAnswer = 'Scissors'
+    } 
+    
+    return computerAnswer;
+}
+
+function playRound (playerSelection, computerSelection){
+    
+    //console.log(checkEntry(playerSelection));
+
+    while (checkEntry (playerSelection) == false){
+        playerSelection = prompt('Incorrect entry'), computerPlay();
+    } 
+        let playerSelectionUpper = playerSelection.toUpperCase();
+        let computerSelectionUpper = computerSelection.toUpperCase();
+
+        //console.log("Computer: " + computerSelectionUpper);
+        //console.log("Human: "+ playerSelectionUpper);
+
+        if (playerSelectionUpper.localeCompare('ROCK') == 0){
+            //console.log("ROCK");
+
+            if (computerSelectionUpper.localeCompare('SCISSORS') == 0){
+                
+                console.log('You win! Rock beats Scissors');
+                return 1;
+            } else if (computerSelectionUpper.localeCompare('PAPER') == 0){
+                
+                console.log('You loose! Paper beats Rock');
+                return 0;
+            } else{
+                console.log('Tie');
+                return 2;
+            }
+        } else if (playerSelectionUpper.localeCompare('PAPER') == 0)   {
+            //console.log("PAPER");
+            if (computerSelectionUpper.localeCompare('ROCK') == 0){
+                
+                console.log('You win! Paper beats Rock');
+                return 1;
+            } else if (computerSelectionUpper.localeCompare('SCISSORS') == 0){
+                
+                console.log('You Loose! Scissors beats Paper');
+                return 0;
+            } else{
+                console.log('Tie');
+                return 2;
+            }
+        } else if (playerSelectionUpper.localeCompare('SCISSORS') == 0){
+            //console.log("SCISSORS");
+            if (computerSelectionUpper.localeCompare('PAPER') == 0){
+                
+                console.log('You win! Scissors beats Paper');
+                return 1;
+            } else if (computerSelectionUpper.localeCompare('ROCK') == 0){
+                console.log('You loose! Rock beats Scissors');
+                return 0;
+            } else{
+                 console.log('Tie');
+                 return 2;
+            }
+        }
+        
+    
+}
+
+function game(rounds){
+    let player = 0;
+    let computer = 0;
+    let round;
+    for (let i = 0; i < rounds; i++){
+        console.log('Round: ' + (i+1));
+        round = playRound(prompt("Rock, Paper or Scissors?"),computerPlay());
+        
+        if (round == 1){
+            player++;
+        } else if (round == 0){
+            computer++;
+        }
+        //console.log(player);
+        //console.log(computer);
+    }
+
+    if (player > computer){
+        console.log("You win the game!");
+    } else if (computer > player){
+        console.log("You loose the game!");
+    } else if (player == computer){
+        console.log("It is a draw");
+    }
+
+
+}
+
+
+
+function checkEntry (playerSelection){
+    
+    
+    let playerSelectionUpper = playerSelection.toUpperCase();
+
+    if (playerSelectionUpper.localeCompare('ROCK') == 0 || playerSelectionUpper.localeCompare('SCISSORS') == 0 || playerSelectionUpper.localeCompare('PAPER') == 0){
+        return true;
+
+    } else {
+        return false;
+    }
+
+
+}
+
+game(5);
+
+//let UserInput = parseInt(prompt("Please enter your selection:"));
+//game(UserInput, computerPlay());
